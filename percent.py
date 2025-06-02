c0dceb4f3fa5aac7caf34657c588c5ce9ef08013
KITTIPHAT WAENGCHAIYAPHUM <119990197+frosty-cell@users.noreply.github.com>
2025-06-02 17:54:22 +0700

    Revert "Update: Add selectable operator method (+ / -)"

--- percent.py
+++ percent.py
@@ -1,18 +1,10 @@
 isr = int(input('input Moodeng => "%" such as 50% Enter |50| : '))
 print(f'number of percent: {isr}%')
 
-rpcP = (isr / 100) + 1
-# print(f'percent factor: {rpcP:.2f}')
-rpcM = (isr / 100)
+rpc = (isr / 100) + 1
+# print(f' percent:  {ry:.2f}'')
 
-based = int(input('based starter: ')) 
-rbP = based * rpcP
-rbM = based - (based * rpcM)
+based = int(input("based starter: ")) 
+rb = based * rpc
 
-selection = str(input('select: minus or plus to solve your program ==> insert [ "-" or "+" ] '))
-if (selection in ['-', 'minus']):
-    print(f'based({based}) - percent({isr}%) = {rbM:.3f}')
-elif (selection in ['+', 'plus']):
-    print(f'based({based}) + percent({isr}%) = {rbP:.3f}')
-else:
-    print('Please Select again (Hint: only insert "-" or "+")')
+print(f'based({based}) + percent({isr}%) = {rb:.3f}')
